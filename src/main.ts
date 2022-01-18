2e4c4e2f77885f8f8751957ab429666bc7f09701
miraenoh <yes_future@naver.com>
2022-01-19 02:57:28 +0900

    feat: ValidationPipe 적용 및 관련 라이브러리 설치

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Pipe 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  );
 
   // Swagger 초기화
   const swaggerConfig = new DocumentBuilder()
